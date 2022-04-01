c23234a03f9cec101acd39ddb5fc7ca5fb237afd
Rich Davis <rdavis@galileosuite.com>
2022-04-01 15:28:25 -0400

    add help

--- common/maps/memory.go
+++ common/maps/memory.go
@@ -1,5 +1,8 @@
 package maps
 
+// Win32_PhysicalMemory describes the physical memory available
+// to the Windows OS.
+// https://docs.microsoft.com/en-us/windows/win32/cimwin32prov/win32-physicalmemory
 type Win32_PhysicalMemory struct {
 	Capacity    uint64
 	Description string
